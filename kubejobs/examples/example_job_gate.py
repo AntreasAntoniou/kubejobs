7ea2fb832056b366d3181fe330a7dbcdd972f922
AntreasAntoniou <iam@antreas.io>
2023-05-26 22:30:02 +0000

    updates

--- kubejobs/examples/example_job_gate.py
+++ kubejobs/examples/example_job_gate.py
@@ -6,7 +6,7 @@ from kubejobs import KubernetesJob, create_jobs_for_experiments, create_pvc
 
 env_vars = {
     "NEPTUNE_API_TOKEN": "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vYXBwLm5lcHR1bmUuYWkiLCJhcGlfdXJsIjoiaHR0cHM6Ly9hcHAubmVwdHVuZS5haSIsImFwaV9rZXkiOiJkOTFjMTY5Zi03ZGUwLTQ4ODYtYWI0Zi1kZDEzNjlkMGI5ZjQifQ==",
-    "NEPTUNE_PROJECT": "MachineLearningBrewery/gate-exp-0-8-6",
+    "NEPTUNE_PROJECT": "MachineLearningBrewery/gate-dev-0-8-0",
     "NEPTUNE_ALLOW_SELF_SIGNED_CERTIFICATE": "TRUE",
     "WANDB_API_KEY": "821661c6ee1657a2717093701ab76574ae1a9be0",
     "WANDB_ENTITY": "machinelearningbrewery",
@@ -22,12 +22,11 @@ env_vars = {
     "CODE_DIR": "/app/",
     "PROJECT_DIR": "/app/",
     "EXPERIMENT_NAME_PREFIX": "gate-exp",
-    "EXPERIMENTS_DIR": "/data/experiments/",
-    "EXPERIMENT_DIR": "/data/experiments/",
+    "EXPERIMENTS_DIR": "/experiments/",
+    "EXPERIMENT_DIR": "/experiments/",
     "TALI_DATASET_DIR": "/tali-data/",
     "WIT_DATASET_DIR": "/tali-data/wit/",
-    "DATASET_DIR": "/data/",
-    "MODEL_DIR": "/data/model/",
+    "MODEL_DIR": "/model/",
 }
 
 # unique id generated using time
@@ -37,18 +36,18 @@ unique_id = time.strftime("%Y%m%d%H%M%S")
 
 
 job = KubernetesJob(
-    name=f"gate-dev-{unique_id}",
+    name=f"gate-node-0-{unique_id}",
     image="ghcr.io/antreasantoniou/gate:latest",
     command=["/bin/bash", "-c", "--"],
-    args=["while true; do sleep 60; done;"],
+    args=["export"],
     gpu_type="nvidia.com/gpu",
     gpu_product="NVIDIA-A100-SXM4-40GB",
-    shm_size="900G",  # "200G" is the maximum value for shm_size
+    shm_size="120G",  # "200G" is the maximum value for shm_size
     gpu_limit=1,
     backoff_limit=4,
     volume_mounts={
         "gate-disk": {
-            "pvc": "gate-pvc-5",
+            "pvc": "datasets-pvc-0",
             "mountPath": "/data/",
         },
     },
